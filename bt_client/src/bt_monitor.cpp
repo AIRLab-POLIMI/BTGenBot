eb789326393ce36b1e42c0cc2a86694092542514
RiccardoIzzo <izzo.riccardo38@gmail.com>
2024-03-30 16:35:59 +0100

    update bt_monitor add file deletion + update main cycle to manage multiple bt sequentially

--- bt_client/src/bt_monitor.cpp
+++ bt_client/src/bt_monitor.cpp
@@ -13,16 +13,15 @@
 #include <memory>
 #include <iostream>
 #include <cstdlib> 
+#include <filesystem>
+#include "yaml-cpp/yaml.h"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp/node_interfaces/node_topics.hpp"
 #include "rcpputils/filesystem_helper.hpp"
 
+
+
 using namespace std::chrono_literals;
-
-const std::string dir = "/BTGenBot/bt_client/bt_xml/";
-
-const std::string tree_xml = "main_tree.xml";
-
 
 
 class FileCheckerNode : public rclcpp::Node
@@ -30,23 +29,43 @@ class FileCheckerNode : public rclcpp::Node
 public:
     FileCheckerNode() : Node("file_checker")
     {
+        // Create a timer to check the file every second
         timer_ = this->create_wall_timer(1000ms, std::bind(&FileCheckerNode::checkFile, this));
+
+        // Get the current directory
+        std::filesystem::path curr_dir = std::filesystem::current_path();
+        // Get the configuration file path
+        std::filesystem::path config_dir = curr_dir / "config/tree.yaml";
+        // Load the configuration file with the tree name
+        YAML::Node config_file = YAML::LoadFile(config_dir);
+
+        // Get the directory of the behavior tree XML file using the name from the configuration file
+        std::filesystem::path tree_dir = curr_dir / "bt_xml/";
+        xml_tree_path_ = tree_dir.string() + config_file["tree_name"].as<std::string>();
+
+        flag_ = true;
     }
 
 private:
     void checkFile()
     {
-        if (rcpputils::fs::exists(dir + tree_xml))
+        if (rcpputils::fs::exists(xml_tree_path_) && flag_)
         {
-            RCLCPP_INFO(this->get_logger(), "File found. Launching behaviour tree client...");
+            // Set the flag to false to avoid multiple launches of the behavior tree client
+            flag_ = false;
+            RCLCPP_INFO(this->get_logger(), "Behavior tree found. Launching behavior tree client...");
+            // Launch the behavior tree client
             std::system("ros2 launch bt_client bt.launch.py");
-            timer_->cancel(); 
-            rclcpp::shutdown(); 
+            // Remove the behavior tree XML file
+            std::remove(xml_tree_path_.c_str());
+            // When the file is removed, set the flag to true and wait for the next file
+            flag_ = true;
         }
     }
 
-    std::string file_path_;
-    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::TimerBase::SharedPtr timer_; // timer to check the file
+    std::string xml_tree_path_; // full path to the XML file
+    bool flag_; // flag to avoid multiple launches of the behavior tree client
 };
 
 int main(int argc, char *argv[])
